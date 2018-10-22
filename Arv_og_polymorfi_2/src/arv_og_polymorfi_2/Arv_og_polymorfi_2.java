db0e311fd702ba303273491043d572a1b19da219
Mazipanen <43139438+Mazipanen@users.noreply.github.com>
2018-10-22 13:09:05 +0200

    Updated
    Added a if statement

---
+++ Arv_og_polymorfi_2/src/arv_og_polymorfi_2/Arv_og_polymorfi_2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arv_og_polymorfi_2;
+
+/**
+ *
+ * @author L530
+ */
+public class Arv_og_polymorfi_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String i = "0";
+        if ("2".equals(i)) {
+            System.out.println("YAY!");
+        }
+        else {
+            System.out.println("Awe :/");
+        }
+    }
+    
+}
